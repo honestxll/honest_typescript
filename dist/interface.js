2750b5612b79670afc79833d78cffc796161a667
honest <honestxll@gmail.com>
2018-09-08 22:26:51 +0800

    'enum'

--- dist/interface.js
+++ dist/interface.js
@@ -2,7 +2,14 @@
 /**
  * TypeScript Interface
  */
+var PostStatus;
+(function (PostStatus) {
+    PostStatus[PostStatus["Unpublished"] = 0] = "Unpublished";
+    PostStatus[PostStatus["Published"] = 1] = "Published";
+    PostStatus[PostStatus["Draft"] = 2] = "Draft";
+})(PostStatus || (PostStatus = {}));
 var post = {
     title: 'One Day',
-    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta eveniet delectus alias facilis, eos recusandae facere architecto dicta maiores, voluptatem dolores, autem. Sequi iste, hic voluptates quibusdam labore vero doloremque.'
+    content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta eveniet delectus alias facilis, eos recusandae facere architecto dicta maiores, voluptatem dolores, autem. Sequi iste, hic voluptates quibusdam labore vero doloremque.',
+    status: PostStatus.Unpublished,
 };
--- src/interface.ts
+++ src/interface.ts
@@ -2,13 +2,21 @@
  * TypeScript Interface
  */
 
+enum PostStatus {
+  Unpublished,
+  Published,
+  Draft,
+}
+
 interface Post {
   title: string;
   content: string;
   author?: string; // 可以不给
+  status: PostStatus;
 }
 
 let post: Post = {
   title: 'One Day',
-  content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta eveniet delectus alias facilis, eos recusandae facere architecto dicta maiores, voluptatem dolores, autem. Sequi iste, hic voluptates quibusdam labore vero doloremque.'
+  content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta eveniet delectus alias facilis, eos recusandae facere architecto dicta maiores, voluptatem dolores, autem. Sequi iste, hic voluptates quibusdam labore vero doloremque.',
+  status: PostStatus.Unpublished,
 }
