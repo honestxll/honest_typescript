2c50b21125c6864ac321b010ccddb12269e5e1e9
honest <honestxll@gmail.com>
2018-09-08 21:57:07 +0800

    '变量类型'

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+/**
+ * TypeScript
+ */
+var lastNight = '🍹🍺🍎🍐';
+var money = 3.2;
+var isDone = false;
+var arr = ['apple', 'pear'];
+// 如果不确定变量的类型可以这样
+var ghost = '👻';
---
+++ src/app.ts
@@ -0,0 +1,11 @@
+/**
+ * TypeScript
+ */
+
+let lastNight: string = '🍹🍺🍎🍐';
+let money: number = 3.2;
+let isDone: boolean = false;
+let arr: string[] = ['apple', 'pear'];
+// 如果不确定变量的类型可以这样
+let ghost: any = '👻';
+let array: Array<string> = ['🍎', '🍐', '🍌'];
